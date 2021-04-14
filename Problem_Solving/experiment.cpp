b8a03faa7c05771cf7925ec61cdf7f58d2504f6d
krsinghshubham <47324260+krsinghshubham@users.noreply.github.com>
2021-04-15 04:54:10 +0530

    cpp setup complete on my Mac

--- Problem_Solving/experiment.cpp
+++ Problem_Solving/experiment.cpp
@@ -1,25 +1,14 @@
-
 #include <bits/stdc++.h>
+
 using namespace std;
-#define ll long long
-#define fo(i, n) for (int i = 0; i < n; i++)
-#define FAST_INPUT_OUTPUT_TEMPLATE_TEMPLATE ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-#define pb push_back
-#define PI 3.1415926535897932384626
-#define INF 1000000000 //10 ^9
 
 int main()
 {
-  clock_t begin=clock();
-  //* --------------------------------
+    vector<string> msg {"Helleo", "C+d+", "World", "from", "VS Code", "and the C++ extension! hdslk  lkjdsl "};
 
-  
-  
-
-
-
-  //*---------------------------------
-  clock_t end=clock();
-  cout<<"\n\nExecute In: "<<double(end-begin)/CLOCKS_PER_SEC<<" ms";
-  return 0;
+    for (auto word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
 }
